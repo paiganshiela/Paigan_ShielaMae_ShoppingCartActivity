13fd2cda5b2e4c14b9f3c02067c869448bb607a8
paiganshiela <106149228+paiganshiela@users.noreply.github.com>
2026-04-15 01:53:25 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+
+class Product
+{
+    public int Id;
+    public string Name;
+    public double Price;
+    public int Stock;
+
+    public void Display()
+    {
+        Console.WriteLine(Id + ". " + Name + ":" + Price + " (Stock: " + Stock + ")");
+    }
+}
+
+class CartItem
+{
+    public Product Product;
+    public int Quantity;
+}
+class Program
+{
+    static void Main()
+    {
+
+        Product[] products = new Product[10];
+
+        products[0] = new Product { Id = 1, Name = "Lipstick", Price = 500, Stock = 20 };
+        products[1] = new Product { Id = 2, Name = "Foundation", Price = 1500, Stock = 10 };
+        products[2] = new Product { Id = 3, Name = "Eyeshadow", Price = 2000, Stock = 30 };
+        products[3] = new Product { Id = 4, Name = "Mascara", Price = 400, Stock = 20 };
+        products[4] = new Product { Id = 5, Name = "Contour", Price = 150, Stock = 10 };
+        products[5] = new Product { Id = 6, Name = "Eyeliner", Price = 210, Stock = 30 };
+        products[6] = new Product { Id = 7, Name = "Lipline", Price = 100, Stock = 20 };
+        products[7] = new Product { Id = 8, Name = "Highlighter", Price = 160, Stock = 10 };
+        products[8] = new Product { Id = 9, Name = "Blush on", Price = 200, Stock = 30 };
+        products[9] = new Product { Id = 10, Name = "Pressed Powder", Price = 600, Stock = 20 };
+       
+
+        CartItem[] cart = new CartItem[10];
+        int cartCount = 0;
+
+        double total = 0;
+
+        while (true)
+        {
+            Console.WriteLine("+=== MENU ===+");
+            for (int i = 0; i < products.Length; i++)
+            {
+                products[i].Display();
+            }
+            Console.WriteLine("11. Exit");
+
+            Console.Write("Enter product number: ");
+            int choice;
+
+            if (!int.TryParse(Console.ReadLine(), out choice))
+            {
+                Console.WriteLine("Invalid input.");
+                continue;
+            }
+
+            if (choice == 0)
+            {
+                break;
+            }
+
+            if (choice < 1 || choice > products.Length)
+            {
+                Console.WriteLine("Invalid product.");
+
+            }
+        }
+    }
+}
