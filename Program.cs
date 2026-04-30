3fe0f885c62609ab10b905fa2f121c133f231d6b
paiganshiela <106149228+paiganshiela@users.noreply.github.com>
2026-04-30 23:48:00 +0800

    draft

--- Program.cs
+++ Program.cs
@@ -1,17 +1,12 @@
 ﻿using System;
 
-
 class Product
 {
     public int Id;
     public string Name;
     public double Price;
     public int Stock;
-
-    public void Display()
-    {
-        Console.WriteLine(Id + ". " + Name + ":" + Price + " (Stock: " + Stock + ")");
-    }
+    public string Category;
 }
 
 class CartItem
@@ -19,6 +14,7 @@ class CartItem
     public Product Product;
     public int Quantity;
 }
+
 class Program
 {
     static void Main()
@@ -26,17 +22,16 @@ class Program
 
         Product[] products = new Product[10];
 
-        products[0] = new Product { Id = 1, Name = "Lipstick", Price = 500, Stock = 20 };
-        products[1] = new Product { Id = 2, Name = "Foundation", Price = 1500, Stock = 10 };
-        products[2] = new Product { Id = 3, Name = "Eyeshadow", Price = 2000, Stock = 30 };
-        products[3] = new Product { Id = 4, Name = "Mascara", Price = 400, Stock = 20 };
-        products[4] = new Product { Id = 5, Name = "Contour", Price = 150, Stock = 10 };
-        products[5] = new Product { Id = 6, Name = "Eyeliner", Price = 210, Stock = 30 };
-        products[6] = new Product { Id = 7, Name = "Lipline", Price = 100, Stock = 20 };
-        products[7] = new Product { Id = 8, Name = "Highlighter", Price = 160, Stock = 10 };
-        products[8] = new Product { Id = 9, Name = "Blush on", Price = 200, Stock = 30 };
-        products[9] = new Product { Id = 10, Name = "Pressed Powder", Price = 600, Stock = 20 };
-
+        products[0] = new Product { Id = 1, Name = "Lipstick", Price = 500, Stock = 20, Category = "Lip" };
+        products[1] = new Product { Id = 2, Name = "Foundation", Price = 1500, Stock = 10, Category = "Face" };
+        products[2] = new Product { Id = 3, Name = "Eyeshadow", Price = 2000, Stock = 30, Category = "Eye" };
+        products[3] = new Product { Id = 4, Name = "Mascara", Price = 400, Stock = 20, Category = "Eye" };
+        products[4] = new Product { Id = 5, Name = "Contour", Price = 150, Stock = 10, Category = "Face" };
+        products[5] = new Product { Id = 6, Name = "Eyeliner", Price = 210, Stock = 30, Category = "Eye" };
+        products[6] = new Product { Id = 7, Name = "Lipline", Price = 100, Stock = 20, Category = "Lip" };
+        products[7] = new Product { Id = 8, Name = "Highlighter", Price = 160, Stock = 10, Category = "Face" };
+        products[8] = new Product { Id = 9, Name = "Blush on", Price = 200, Stock = 30, Category = "Face" };
+        products[9] = new Product { Id = 10, Name = "Pressed Powder", Price = 600, Stock = 20, Category = "Face" };
 
         CartItem[] cart = new CartItem[10];
         int cartCount = 0;
@@ -45,103 +40,20 @@ class Program
 
         while (true)
         {
-            Console.WriteLine("+=== MENU ===+");
-            for (int i = 0; i < products.Length; i++)
+            while (true)
             {
-                products[i].Display();
+                Console.WriteLine("\n=== MENU ===");
+                Console.WriteLine("1. View Products");
+                Console.WriteLine("2. Search");
+                Console.WriteLine("3. Product Category");
+                Console.WriteLine("4. View Cart");
+                Console.WriteLine("5. Checkout");
+                Console.WriteLine("6. Exit");
+
+                Console.Write("Choice: ");
+                int choice = int.Parse(Console.ReadLine());
+
             }
-            Console.WriteLine("11. Exit");
-            Console.WriteLine();
-
-            Console.Write("Enter product number: ");
-            int choice;
-
-            if (!int.TryParse(Console.ReadLine(), out choice))
-            {
-                Console.WriteLine("Invalid input.");
-                continue;
-            }
-
-            if (choice == 11)
-            {
-                break;
-            }
-
-            if (choice < 1 || choice > products.Length)
-            {
-                Console.WriteLine("Invalid product");
-                continue;
-            }
-
-            Product selected = products[choice - 1];
-
-            if (selected.Stock == 0)
-            {
-                Console.WriteLine("Out of stock");
-                continue;
-            }
-
-            Console.Write("Enter quantity: ");
-            int qty;
-
-            if (!int.TryParse(Console.ReadLine(), out qty) || qty <= 0)
-            {
-                Console.WriteLine("Invalid quantity");
-                continue;
-            }
-
-            if (qty > selected.Stock)
-            {
-                Console.WriteLine("Not enough stock");
-                continue;
-            }
-
-            double itemTotal = selected.Price * qty;
-            total += itemTotal;
-            selected.Stock -= qty;
-
-            cart[cartCount] = new CartItem
-            {
-                Product = selected,
-                Quantity = qty
-            };
-
-            cartCount++;
-
-            Console.WriteLine("Added to cart!");
-            Console.WriteLine("Subtotal: " + itemTotal);
-
-            Console.Write("Add more? (N/Y): ");
-            string ans = Console.ReadLine();
-            Console.WriteLine();
-
-            if (ans.ToUpper() == "N")
-                break;
-        }
-        {
-            Console.WriteLine();
-        }
-
-        Console.WriteLine("+==== TOTAL ====+");
-        Console.WriteLine("Grand Total: " + total);
-
-        if (total >= 5000)
-        {
-            double discount = total * 0.10;
-            double finalTotal = total - discount;
-
-            Console.WriteLine("Discount: " + discount);
-            Console.WriteLine("Final Total: " + finalTotal);
-        }
-        {
-            Console.WriteLine();
-        }
-
-
-        Console.WriteLine("+=== REMAINING STOCK ===+");
-        for (int i = 0; i < products.Length; i++)
-        {
-            Console.WriteLine(products[i].Name + ": " + products[i].Stock);
         }
     }
 }
